6160a74a6c5114184cd0a2955cd3dec374a91a90
Haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2024-08-07 15:29:19 +0700

    Arrow Functions

---
+++ Pertemuan070824/2_arrowFunctions.js
@@ -0,0 +1,9 @@
+// Fungsi biasa
+function bio(umur) {
+    return `Saya berumur ${umur}`;
+  }
+  
+// Arrow function
+const bio = (umur) => `Saya berumur ${umur}`;
+  
+console.log(bio(17)); // Output: Halo, Mirza
