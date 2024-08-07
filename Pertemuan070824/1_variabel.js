2ccf2f612fbc5f7bdbf786263edf44ea0130075b
Haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2024-08-07 15:18:49 +0700

    Variabel

---
+++ Pertemuan070824/1_variabel.js
@@ -0,0 +1,10 @@
+// Menggunakan let
+let nama = "Arif";
+console.log(nama); // Output: Arif
+nama = "Fayyadh Rantisi";
+console.log(nama); // Output: Fayyadh Rantisi
+
+// Menggunakan const
+const doorprice = 87;
+console.log(doorprice); // Output: 87
+// doorprice = 78; // Error: Assignment to constant variable
