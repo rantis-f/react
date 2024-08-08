3faba5e9aff88c5d373b8a5df2f795c7826a3c52
Haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2024-08-08 08:49:53 +0700

    Modules (import/export)

---
+++ Pertemuan070824/6_ modulo.js
@@ -0,0 +1,5 @@
+function modulo(a, b) {
+    return a % b;
+  }
+const reedom = 43;
+module.exports={modulo,reedom}
---
+++ Pertemuan070824/6_main.js
@@ -0,0 +1,3 @@
+const { modulo, reedom } = require('./6_ modulo.js');
+console.log(modulo(65, 3)); // Output: 2
+console.log(reedom); // Output: 43
