0f4ba9afda7553225b42ca92fe922a23df6453a8
Haempebe <hamidzanpranajanalabhamkerti@gmail.com>
2024-08-08 11:08:06 +0700

    Penyesuaian nama file

--- Pertemuan070824/6_main.js
+++ Pertemuan070824/6_main.js
@@ -1,3 +1,3 @@
-const { modulo, reedom } = require('./6_ modulo.js');
+const { modulo, reedom } = require('./6_modulo.js');
 console.log(modulo(65, 3)); // Output: 2
 console.log(reedom); // Output: 43
